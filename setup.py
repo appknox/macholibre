b3e2ea0b457d3536cebe171ab0328fd6fdfd74f0
Praseetha-KR <praseetha04@gmail.com>
2019-10-30 14:57:48 +0530

    Bump version: 1.1.0-part0 → 1.1.0-part1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = open(os.path.join(here, 'README.rst')).read()
 NEWS = open(os.path.join(here, 'NEWS.txt')).read()
 
 
-version = '1.1.0-part0'
+version = '1.1.0.post1'
 
 
 install_requires = [
